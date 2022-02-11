f061b0aff1c8316463e597c0ec6ba2e36930294f
fsampson30 <fsampson30@gmail.com>
2022-02-11 10:31:38 -0300

    Initial commit

---
+++ app/src/main/java/com/sampson/kotlinrxjava/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.sampson.kotlinrxjava
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
